90148f46fad1ee9fdd468f750df10f13d19b3b72
Milos Milisavljevic <milos.milisavljevic@symphony.is>
2023-10-16 01:08:01 +0200

    pass tests

---
+++ test/app.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../src/app.js');  // Import your Express app
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe('GET /', () => {
+  it('should return status 200', (done) => {
+    chai.request(app)
+      .get('/')  // Replace with your actual route
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
